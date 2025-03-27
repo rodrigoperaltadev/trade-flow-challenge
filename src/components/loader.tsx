6ed89a280d5eb2b0bcca17c9fedfee352fd4dba4
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-27 13:00:05 -0300

    feat: add loader component

---
+++ src/components/loader.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { LottieLoader, LottieLoaderProps } from 'lottie-loader-react-native';
+import { lottieLoader } from '../assets/lottie';
+
+interface LoaderProps {
+  visible: boolean;
+  animationStyle?: LottieLoaderProps;
+}
+
+export const Loader: React.FC<LoaderProps> = ({ visible, animationStyle }) => {
+  return (
+    <LottieLoader
+      visible={visible}
+      source={lottieLoader}
+      speed={1}
+      loop
+      style={styles.lottie}
+      {...animationStyle}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  lottie: { height: 100, width: 100 }
+});
