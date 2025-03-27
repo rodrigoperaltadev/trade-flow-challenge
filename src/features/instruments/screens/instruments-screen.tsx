5a93b4a77f0f1503a2c658092c1b7a649151b725
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-26 21:35:13 -0300

    feat: implements useInstruments in screen

--- src/features/instruments/screens/instruments-screen.tsx
+++ src/features/instruments/screens/instruments-screen.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { useInstruments } from '../hooks/use-instruments';
+import { ThemedText } from '../../../components/themed-text';
+import { ThemedView } from '../../../components/themed-view';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function InstrumentsScreen() {
+  const { data, isLoading, error } = useInstruments();
+
+  if (isLoading) return <ThemedText>Loading...</ThemedText>;
+  if (error) return <ThemedText>Error loading instruments</ThemedText>;
+
   return (
-    <View style={styles.container}>
-      <Text>Instruments Screen</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <ThemedView style={styles.container}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.ticker}
+          renderItem={({ item }) => (
+            <View style={styles.itemContainer}>
+              <ThemedText style={styles.itemText}>
+                {item.ticker} - {item.name}
+              </ThemedText>
+              <ThemedText>Último Precio: ${item.last_price}</ThemedText>
+              <ThemedText>
+                Retorno: {(item.last_price - item.close_price).toFixed(2)}
+              </ThemedText>
+            </View>
+          )}
+        />
+      </ThemedView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center'
+    padding: 16
+  },
+  itemContainer: {
+    marginBottom: 16
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  safeAreaContainer: {
+    flex: 1
   }
 });
