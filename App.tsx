bfe297ad3605ebb1bcd58c1ad84c3d566fbf238a
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-26 21:37:29 -0300

    feat: add themed view

--- App.tsx
+++ App.tsx
@@ -2,15 +2,25 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { AppNavigator } from './src/navigation/app-navigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { queryClient } from './src/services/query-client';
+import { ThemedView } from './src/components/themed-view';
+import { StyleSheet } from 'react-native';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <AppNavigator />
+        <ThemedView style={styles.container}>
+          <AppNavigator />
+        </ThemedView>
       </NavigationContainer>
     </QueryClientProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default App;
