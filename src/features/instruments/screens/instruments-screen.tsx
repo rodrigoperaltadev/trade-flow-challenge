d287abb8f0d372491d62c2302ad318d1b47ba14c
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-27 10:51:34 -0300

    feat: implements order modal

--- src/features/instruments/screens/instruments-screen.tsx
+++ src/features/instruments/screens/instruments-screen.tsx
@@ -1,36 +1,51 @@
-import React from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, StyleSheet, Button } from 'react-native';
 import { useInstruments } from '../hooks/use-instruments';
 import { ThemedText } from '../../../components/themed-text';
 import { ThemedView } from '../../../components/themed-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import OrderModal from '../components/order-modal';
+import { Instrument } from '../../../types/instrument';
 
 export default function InstrumentsScreen() {
   const { data, isLoading, error } = useInstruments();
+  const [selectedInstrument, setSelectedInstrument] =
+    useState<Instrument | null>(null);
 
   if (isLoading) return <ThemedText>Loading...</ThemedText>;
   if (error) return <ThemedText>Error loading instruments</ThemedText>;
 
   return (
-    <SafeAreaView style={styles.safeAreaContainer}>
-      <ThemedView style={styles.container}>
-        <FlatList
-          data={data}
-          keyExtractor={(item) => item.ticker}
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-              <ThemedText style={styles.itemText}>
-                {item.ticker} - {item.name}
-              </ThemedText>
-              <ThemedText>Último Precio: ${item.last_price}</ThemedText>
-              <ThemedText>
-                Retorno: {(item.last_price - item.close_price).toFixed(2)}
-              </ThemedText>
-            </View>
-          )}
-        />
-      </ThemedView>
-    </SafeAreaView>
+    <React.Fragment>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <ThemedView style={styles.container}>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.ticker}
+            renderItem={({ item }) => (
+              <View style={styles.itemContainer}>
+                <ThemedText style={styles.itemText}>
+                  {item.ticker} - {item.name}
+                </ThemedText>
+                <ThemedText>Último Precio: ${item.last_price}</ThemedText>
+                <ThemedText>
+                  Retorno: {(item.last_price - item.close_price).toFixed(2)}
+                </ThemedText>
+                <Button
+                  title="Ordenar"
+                  onPress={() => setSelectedInstrument(item)}
+                />
+              </View>
+            )}
+          />
+        </ThemedView>
+      </SafeAreaView>
+      <OrderModal
+        visible={selectedInstrument !== null}
+        onClose={() => setSelectedInstrument(null)}
+        instrument={selectedInstrument}
+      />
+    </React.Fragment>
   );
 }
 
