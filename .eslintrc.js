23e8600f3a336841bb6fef2e49f5298a1dc16c28
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-26 21:31:57 -0300

    refactor: fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,36 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
-    project: "./tsconfig.json",
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   env: {
-    "react-native/react-native": true,
-    es6: true,
+    'react-native/react-native': true,
+    es6: true
   },
   extends: [
-    "expo",
-    "plugin:react/recommended",
-    "plugin:react-native/all",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'expo',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
   ],
   plugins: [
-    "react",
-    "react-native",
-    "@typescript-eslint",
-    "react-hooks",
-    "prettier",
+    'react',
+    'react-native',
+    '@typescript-eslint',
+    'react-hooks',
+    'prettier'
   ],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
 
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "no-console": "warn",
-  },
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'react-native/no-raw-text': 'off'
+  }
 };
