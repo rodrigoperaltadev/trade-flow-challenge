fcffa9487c665b2286b2a44b5c18ec45bf4804fc
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 00:26:00 -0300

    feat: add types to mapper

--- src/types/instrument.ts
+++ src/types/instrument.ts
@@ -1,7 +1,11 @@
-export interface Instrument {
+export interface InstrumentResponse {
   id: number;
   name: string;
   ticker: string;
   last_price: number;
   close_price: number;
+}
+
+export interface Instrument extends InstrumentResponse {
+  return_percentage: number;
 }
