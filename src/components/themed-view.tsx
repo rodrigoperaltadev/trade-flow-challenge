ba0d61d1025068f1baf32c5a7e9eaccda85a0dd6
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 14:09:02 -0300

    feat: change theme view component

--- src/components/themed-view.tsx
+++ src/components/themed-view.tsx
@@ -1,9 +1,29 @@
 import React, { FC } from 'react';
 import { View, ViewProps } from 'react-native';
 import { useTheme } from '../theme/useTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export const ThemedView: FC<ViewProps> = ({ style, ...props }) => {
+type ThemedViewProps = ViewProps & {
+  safeArea?: boolean;
+};
+
+export const ThemedView: FC<ThemedViewProps> = ({
+  style,
+  safeArea,
+  ...props
+}) => {
   const { background } = useTheme();
+  const { top, bottom } = useSafeAreaInsets();
+  const paddingTop = safeArea ? top : 0;
+  const paddingBottom = safeArea ? bottom : 0;
 
-  return <View style={[{ backgroundColor: background }, style]} {...props} />;
+  return (
+    <View
+      style={[
+        { backgroundColor: background, paddingTop, paddingBottom },
+        style
+      ]}
+      {...props}
+    />
+  );
 };
