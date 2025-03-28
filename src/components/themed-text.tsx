aa67c686e30ba4f7a4186fc62352c2e195884385
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 14:06:19 -0300

    feat: change themed text implementation

--- src/components/themed-text.tsx
+++ src/components/themed-text.tsx
@@ -1,9 +1,36 @@
 import React, { FC } from 'react';
 import { useTheme } from '../theme/useTheme';
 import { Text, TextProps } from 'react-native';
+import { COLORS } from '../theme/colors';
 
-export const ThemedText: FC<TextProps> = ({ style, ...props }) => {
+type TextType = 'success' | 'error' | 'info' | 'neutral';
+
+type ThemedTextProps = TextProps & {
+  type?: TextType;
+  size?: number;
+  fontWeight?: 'normal' | 'bold';
+};
+
+export const ThemedText: FC<ThemedTextProps> = ({
+  style,
+  type = 'neutral',
+  size,
+  fontWeight = 'normal',
+  ...props
+}) => {
   const { text } = useTheme();
 
-  return <Text style={[{ color: text }, style]} {...props} />;
+  const textColor: Record<TextType, string> = {
+    success: COLORS.success,
+    error: COLORS.error,
+    info: COLORS.primary,
+    neutral: text
+  };
+
+  return (
+    <Text
+      style={[{ color: textColor[type], fontSize: size, fontWeight }, style]}
+      {...props}
+    />
+  );
 };
