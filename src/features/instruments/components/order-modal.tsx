1fd41a21760c2686b9944a05bbe584ebec3f7484
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 00:22:58 -0300

    feat: improve modal implementation

--- src/features/instruments/components/order-modal.tsx
+++ src/features/instruments/components/order-modal.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
+import React, { FC, useEffect, useState } from 'react';
+import { View, TextInput, Alert, StyleSheet } from 'react-native';
 import { useOrder } from '../hooks/use-order';
-import { OrderRequest } from '../../../types/order';
+import { OrderMode, OrderRequest, OrderType } from '../../../types/order';
 import { Instrument } from '../../../types/instrument';
 import { ModalLayout } from '../../../layouts/modal-layout';
 import { ThemedText } from '../../../components/themed-text';
+import { ButtonGroup } from '../../../components/button-group';
+import { COLORS } from '../../../theme/colors';
+import { Button } from '../../../components/button';
 
 interface OrderModalProps {
   visible: boolean;
@@ -12,19 +15,33 @@ interface OrderModalProps {
   instrument: Instrument | null;
 }
 
-export default function OrderModal({
+export const OrderModal: FC<OrderModalProps> = ({
   visible,
   onClose,
   instrument
-}: OrderModalProps) {
+}) => {
   const [quantity, setQuantity] = useState('');
-  const [orderType, setOrderType] = useState<'BUY' | 'SELL'>('BUY');
-  const [orderMode, setOrderMode] = useState<'MARKET' | 'LIMIT'>('MARKET');
+  const [orderType, setOrderType] = useState<OrderType>('BUY');
+  const [orderMode, setOrderMode] = useState<OrderMode>('MARKET');
   const [price, setPrice] = useState<string>('');
+
+  const orderTypesButtons: OrderType[] = ['BUY', 'SELL'];
+
+  const handleResetValues = () => {
+    setQuantity('');
+    setOrderType('BUY');
+    setOrderMode('MARKET');
+    setPrice('');
+  };
 
   const { mutate, isPending } = useOrder();
 
-  if (!instrument) return null; // 🔥 No renderizar si no hay instrumento seleccionado
+  useEffect(() => {
+    if (visible) return;
+    handleResetValues();
+  }, [visible]);
+
+  if (!instrument) return null;
 
   const handleOrder = () => {
     const orderData: OrderRequest = {
@@ -46,69 +63,87 @@ export default function OrderModal({
     });
   };
 
+  const isInvalid = () => {
+    if (!quantity || parseInt(quantity) < 1) return true;
+    if (orderMode === 'LIMIT' && !price) return true;
+    if (orderMode === 'LIMIT' && parseInt(price) < 0) return true;
+    return false;
+  };
+
   return (
     <ModalLayout visible={visible} onClose={onClose}>
-      <ThemedText style={{ fontSize: 18, fontWeight: 'bold' }}>
-        Orden para {instrument.name} ({instrument.ticker})
-      </ThemedText>
+      <View style={styles.contentContainer}>
+        <View>
+          <ThemedText style={styles.title}>
+            Orden para {instrument.name}
+          </ThemedText>
+          <ThemedText style={styles.title}>({instrument.ticker})</ThemedText>
+        </View>
 
-      <ThemedText>Último Precio: ${instrument.last_price}</ThemedText>
+        <View style={styles.buttonGroupContainer}>
+          <ThemedText>Tipo de Orden:</ThemedText>
 
-      <ThemedText>Tipo de Orden:</ThemedText>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Button
-          title="BUY"
-          onPress={() => setOrderType('BUY')}
-          color={orderType === 'BUY' ? 'green' : 'gray'}
-        />
-        <Button
-          title="SELL"
-          onPress={() => setOrderType('SELL')}
-          color={orderType === 'SELL' ? 'red' : 'gray'}
-        />
-      </View>
-
-      <ThemedText>Modo:</ThemedText>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Button
-          title="MARKET"
-          onPress={() => setOrderMode('MARKET')}
-          color={orderMode === 'MARKET' ? 'blue' : 'gray'}
-        />
-        <Button
-          title="LIMIT"
-          onPress={() => setOrderMode('LIMIT')}
-          color={orderMode === 'LIMIT' ? 'blue' : 'gray'}
-        />
-      </View>
-
-      <ThemedText>Cantidad:</ThemedText>
-      <TextInput
-        keyboardType="numeric"
-        value={quantity}
-        onChangeText={setQuantity}
-        style={{ borderBottomWidth: 1, marginBottom: 10 }}
-      />
-
-      {orderMode === 'LIMIT' && (
-        <>
-          <ThemedText>Precio:</ThemedText>
-          <TextInput
-            keyboardType="numeric"
-            value={price}
-            onChangeText={setPrice}
-            style={{ borderBottomWidth: 1, marginBottom: 10 }}
+          <ButtonGroup
+            buttons={orderTypesButtons}
+            onButtonPress={(item) => setOrderType(item as OrderType)}
+            selectedButton={orderType}
           />
-        </>
-      )}
-      <View style={{ gap: 10 }}>
-        <Button
-          title={isPending ? 'Enviando...' : 'Enviar Orden'}
-          onPress={handleOrder}
-          disabled={isPending}
+        </View>
+        <View style={styles.buttonGroupContainer}>
+          <ThemedText>Modo:</ThemedText>
+          <ButtonGroup
+            buttons={['MARKET', 'LIMIT']}
+            onButtonPress={(item) => setOrderMode(item as OrderMode)}
+            selectedButton={orderMode}
+          />
+        </View>
+
+        <ThemedText>Cantidad:</ThemedText>
+        <TextInput
+          keyboardType="numeric"
+          value={quantity}
+          onChangeText={setQuantity}
+          style={styles.textInput}
         />
-        <Button title="Cerrar" onPress={onClose} />
+
+        {orderMode === 'LIMIT' && (
+          <>
+            <ThemedText>Precio:</ThemedText>
+            <TextInput
+              keyboardType="numeric"
+              value={price}
+              onChangeText={setPrice}
+              style={styles.textInput}
+            />
+          </>
+        )}
+        <Button
+          label="Enviar Orden"
+          onPress={handleOrder}
+          disabled={isInvalid()}
+          isPending={isPending}
+        />
       </View>
     </ModalLayout>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  buttonGroupContainer: {
+    gap: 8
+  },
+  contentContainer: {
+    flex: 1,
+    gap: 16
+  },
+  textInput: {
+    backgroundColor: COLORS.primaryLight,
+    borderRadius: 16,
+    fontSize: 14,
+    marginBottom: 10,
+    padding: 12
+  },
+  title: { fontSize: 18, fontWeight: 'bold' }
+});
+
+export default OrderModal;
