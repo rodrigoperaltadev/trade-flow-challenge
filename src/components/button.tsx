d07f5885277be57efe4d43e6f7d5f95b2c7237bb
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 00:19:49 -0300

    feat: add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TextProps,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from 'react-native';
+import { COLORS } from '../theme/colors';
+
+type ButtonProps = TouchableOpacityProps & {
+  isPending?: boolean;
+  label: string;
+  textProps?: TextProps;
+};
+
+export const Button: FC<ButtonProps> = ({
+  isPending = true,
+  label,
+  textProps,
+  ...buttonProps
+}) => {
+  const styleButton = {
+    backgroundColor: buttonProps.disabled ? COLORS.grey : COLORS.primary,
+    opacity: buttonProps.disabled ? 0.5 : 1
+  };
+
+  return (
+    <TouchableOpacity
+      style={{
+        ...styleButton,
+        ...styles.container
+      }}
+      {...buttonProps}>
+      {isPending && <ActivityIndicator color={'white'} size={20} />}
+      <Text style={styles.buttonText} {...textProps}>
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonText: {
+    color: COLORS.white,
+    fontSize: 16
+  },
+  container: {
+    alignItems: 'center',
+    borderRadius: 16,
+    padding: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 8
+  }
+});
