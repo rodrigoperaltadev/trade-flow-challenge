8d14b9a3f0bc0910f436ae317114f9cfbd3125b2
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-03-28 14:08:39 -0300

    feat: add custom status bar

--- App.tsx
+++ App.tsx
@@ -4,15 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { queryClient } from './src/services/query-client';
 import { ThemedView } from './src/components/themed-view';
 import { StyleSheet } from 'react-native';
+import { CustomStatusBar } from './src/features/instruments/components/custom-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <ThemedView style={styles.container}>
-          <AppNavigator />
-        </ThemedView>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <CustomStatusBar />
+          <ThemedView style={styles.container}>
+            <AppNavigator />
+          </ThemedView>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 };
